e8ff9a09a2bc1a02230f45a6791351ad52bbd427
Tyler Shifflett <tylershifflett@Tylers-MacBook-Pro.local>
2022-07-02 14:14:47 -0400

    Remove Config Folder and Update Server for Mongoose

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const db = require("./config/connection");
 const routes = require("./routes");
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001; 
@@ -8,5 +8,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || '', {
+    useNewURLParser: true, 
+    useUnifiedTopology: true
+});
+
+monogoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
