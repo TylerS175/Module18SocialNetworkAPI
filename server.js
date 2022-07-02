8b1e3b0f279a4348de4ffb00bd6c6af32b24f53d
Tyler Shifflett <tylershifflett@Tylers-MacBook-Pro.local>
2022-07-02 14:02:38 -0400

    Creating Server.JS

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3001; 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(routes);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
